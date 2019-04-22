0ccddb3b7a58bda3d57b33981b83e65864c12da8
admly <admly@interia.pl>
2019-04-22 20:05:17 +0200

    81 adding navigation with ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @Output() whatToDisplay = new EventEmitter<string>();
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+
+  display(display: string) {
+    this.whatToDisplay.emit(display);
+  }
 }
