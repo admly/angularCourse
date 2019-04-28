acd34375b8768df11067d933b8a4eb834671109d
admly <admly@interia.pl>
2019-04-28 11:37:40 +0200

    96. Building a Structural Directive

--- directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,23 +1,26 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
   @HostBinding('style.backgroundColor') backgroundColor: string;
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'blue';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseover') mouseover(eventData: Event) {
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
 
   }
 }
