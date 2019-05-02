e71f44b12a1e28474a7abb1a97a30a30940a9fe7
admly <admly@interia.pl>
2019-05-02 18:56:50 +0200

    137 finished

--- routing-start/routing-start/src/app/servers/server/server.component.ts
+++ routing-start/routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,14 +9,18 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit() {
     const param = +this.route.snapshot.parent.params['id'];
     this.server = this.serversService.getServer(param);
-    this.route.params.subscribe((params) =>     this.server = this.serversService.getServer(+params['id']));
+    this.route.params.subscribe((params) => this.server = this.serversService.getServer(+params['id']));
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+  }
 }
