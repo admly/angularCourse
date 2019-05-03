aa6d5a111a72a70442d10caa9c99418a386ad9c6
admly <admly@interia.pl>
2019-05-03 16:27:02 +0200

    193. TD: Using Form Data

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer: any;
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -23,15 +31,23 @@ export class AppComponent {
     //   questionAnswer: '',
     //   gender: 'male'
     // });
-    this.singupForm.form.patchValue({userData: {
+    this.singupForm.form.patchValue({
+      userData: {
         username: suggestedName
-      }});
+      }
+    });
   }
 
   // onSubmit(f: NgForm) {
   //   console.log(f);
   // }
   onSubmit() {
+    this.user.username = this.singupForm.value.username;
+    this.user.email = this.singupForm.value.email;
+    this.user.secretQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+    this.submitted = true;
     console.log(this.singupForm);
   }
 }
