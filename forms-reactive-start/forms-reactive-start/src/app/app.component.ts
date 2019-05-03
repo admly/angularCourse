05daadc944dfda6d02d4d885855fad5ce8c5a602
admly <admly@interia.pl>
2019-05-03 17:21:13 +0200

    208 Reactive: Reacting to status or value changes

--- forms-reactive-start/forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/forms-reactive-start/src/app/app.component.ts
@@ -21,6 +21,7 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    this.signupForm.valueChanges.subscribe((value) => console.log(value));
   }
 
   onSubmit() {
