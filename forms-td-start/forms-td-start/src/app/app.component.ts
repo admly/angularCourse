a7bf14b75078053e0904976663cfbc7f4126a838
admly <admly@interia.pl>
2019-05-03 16:15:46 +0200

    191. TD: Handling radio buttons

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   @ViewChild('f') singupForm: NgForm;
   defaultQuestion = 'pet';
   answer: any;
+  genders = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
